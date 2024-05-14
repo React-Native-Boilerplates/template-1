76a5c944a7ef4ceff6db303765597ceaf4261135
ProgrammerJayT <theanthem8@gmail.com>
2024-05-14 12:25:52 +0200

    Add method onCreate

--- android/app/src/main/java/com/template1/MainActivity.java
+++ android/app/src/main/java/com/template1/MainActivity.java
@@ -7,7 +7,8 @@ import com.facebook.react.ReactRootView;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -16,7 +17,8 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView
+   * is created and
    * you can specify the rendered you wish to use (Fabric or the older renderer).
    */
   @Override
@@ -36,5 +38,10 @@ public class MainActivity extends ReactActivity {
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
     }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
